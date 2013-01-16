a1ed03da11cd3f906ced2ac9851ce1504a803b7b
alonzorz <alonzorz1@gmail.com>
2013-01-15 20:12:06 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Likeme::Application.config.secret_token = 'd6f71e73ff7aeaf2c19964a5e2b1df74bfaf050a6578ebe23df2dc680a721cbd533750e306e9f5ecf1f0f87c11926a71afb114f4e704ad8553248523aa6f40ec'
