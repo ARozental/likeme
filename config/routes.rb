6bffcc251f58e74f79c095e44e0f850b9fbea804
alonzorz <alonzorz1@gmail.com>
2013-01-18 10:04:56 +0200

    login1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Likeme::Application.routes.draw do
+  
+  resources :users
+
+  match 'auth/:provider/callback', to: 'sessions#create'
+  match 'auth/failure', to: redirect('/')
+  match 'signout', to: 'sessions#destroy', as: 'signout'
+  
+  
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +57,7 @@ Likeme::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
