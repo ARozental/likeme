86dabf819de64c8d12f70a937d2dd7fee3a15aa3
alonzorz <alonzorz1@gmail.com>
2013-01-18 10:05:03 +0200

    login1

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,16 @@
+class HomeController < ApplicationController
+  def index
+    #@graph = Koala::Facebook::API.new
+    #@obj = @graph.get_object("koppel")
+    #@pgraph = Koala::Facebook::API.new("AAAFHkL4f2AwBAHdnB19h4jsjkdaZC3e5J1WeXCkrA5opEcy9slZBebY7ZAZCJVIB8I9hpMPkWIZBGCK09auVU7cnFT8zh03mHj8XPpI99ogZDZD")
+    #@pobj = @pgraph.get_connections("me", "books")  
+    #@pobj = @pgraph.get_object("me")
+    
+    
+    @current_user = current_user
+    @user_graph = Koala::Facebook::API.new(current_user.oauth_token)
+    @me = @user_graph.get_object("me")
+    @my_likes = @user_graph.get_connections("me", "likes")
+    
+  end
+end
