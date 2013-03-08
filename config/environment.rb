41c8a2abf13c3db3b3e99951eda590c60ff2ec72
alonzorz <alonzorz1@gmail.com>
2013-03-08 11:23:43 +0200

    h

--- config/environment.rb
+++ config/environment.rb
@@ -5,4 +5,4 @@ require File.expand_path('../application', __FILE__)
 Likeme::Application.initialize!
 
 #try big int integer
-ActiveRecord::ConnectionAdapters::Mysql2Adapter::NATIVE_DATABASE_TYPES[:primary_key] = "BIGINT(8) UNSIGNED DEFAULT NULL auto_increment PRIMARY KEY"
+#ActiveRecord::ConnectionAdapters::Mysql2Adapter::NATIVE_DATABASE_TYPES[:primary_key] = "BIGINT(8) UNSIGNED DEFAULT NULL auto_increment PRIMARY KEY"
