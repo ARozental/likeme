0f246fd76e5d11a756bb6fc4c57daae79c831255
alonzorz <alonzorz1@gmail.com>
2013-03-24 18:27:30 +0200

    bootstrap1

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
   def picture_link(id,size)
-    size = "#{size}"
+    size = "#{size}" #resolution is 120px
     id = "#{id}"
-    html = "<a href=\"http://www.facebook.com/" + id + "\"><img src=\"https://graph.facebook.com/" + id + "/picture?width=200&height=200\" width=" + size + " height=" + size + "></a>"
+    html = "<a href=\"http://www.facebook.com/" + id + "\"><img src=\"https://graph.facebook.com/" + id + "/picture?width=120&height=120\" width=" + size + " height=" + size + "></a>"
     html.html_safe
   end
   
