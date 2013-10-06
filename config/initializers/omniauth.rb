b8dd32ba5e8fb0c59e17a0d2494fd43c28c086b6
alonzorz <alonzorz1@gmail.com>
2013-10-06 16:41:47 +0200

    wtf

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,8 +3,8 @@ OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, '360161967331340', 'd6900a8fcba7fc5ccfba40acff7a3967',
   :iframe => true,
-  :auth_type => 'https',
-  :secure_image_url => true,
+  #:auth_type => 'https',
+  #:secure_image_url => true,
   scope: %W( email user_about_me  user_activities  user_birthday  user_groups  user_hometown  user_interests  user_likes 
              user_location  user_relationships  user_relationship_details  user_religion_politics user_events
              friends_about_me  friends_activities  friends_birthday  friends_groups  friends_hometown  
